49a90f77bb837fb19befd100904d8c51a57f108e
mssxmt <orela.rec@gmail.com>
2024-05-18 00:14:55 +0900

    supabase clientファイル

---
+++ utils/supabaseClient.ts
@@ -0,0 +1,16 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+/**
+ * 
+ * Supabase には 2 つの異なるタイプのクライアントがあります。
+
+クライアント コンポーネント クライアント- ブラウザで実行されるクライアント コンポーネントから Supabase にアクセスします。
+サーバー コンポーネント クライアント- サーバー上でのみ実行されるサーバー コンポーネント、サーバー アクション、およびルート ハンドラーから Supabase にアクセスします。
+ */
+export function createClient() {
+  // Create a supabase client on the browser with project's credentials
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
