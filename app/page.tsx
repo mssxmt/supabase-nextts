8352fbfba7f69c51ef2381bc68c228911ffc5562
mssxmt <orela.rec@gmail.com>
2024-05-18 23:23:55 +0900

    トップページ

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,10 @@
-'use client';
-import { useState, useEffect, Suspense } from 'react';
-import Auth from '../components/Auth';
-import Account from '../components/Account';
-import { Session } from '@supabase/supabase-js';
-import { supabase } from '@/libs/supabaseClient';
+import Link from 'next/link';
 
 export default function Home() {
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    // セッションデータを返す
-    (async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-
-      setSession(session);
-    })();
-
-    // 認証のイベントを受け取る
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-
   return (
-    <main className='container' style={{ padding: '50px 0 100px 0' }}>
-      {!session ? (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Auth />
-        </Suspense>
-      ) : (
-        <Account key={session.user.id} session={session} />
-      )}
+    <main className="container" style={{ padding: '50px 0 100px 0' }}>
+      <Link href="/login">ログインページ</Link>
+      <Link href="/account">Accountページ</Link>
     </main>
   );
 }
