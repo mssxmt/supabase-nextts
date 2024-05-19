ebe621b8f2bb7cc8f04d6a774406b756b9c4b61c
mssxmt <orela.rec@gmail.com>
2024-05-19 10:40:08 +0900

    型引数を追加<Database>

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,3 +1,4 @@
+import { Database } from '@/types/supabase';
 import { createBrowserClient } from '@supabase/ssr';
 
 /**
@@ -9,8 +10,9 @@ import { createBrowserClient } from '@supabase/ssr';
  */
 export function createClient() {
   // Create a supabase client on the browser with project's credentials
-  return createBrowserClient(
+  //Databaseの型を渡す！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
 }
