56bc7362435f45a613bc7d3a6753cd37360f747e
mssxmt <orela.rec@gmail.com>
2024-05-19 10:44:51 +0900

    シングルクォート

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,10 +3,10 @@ import { login, signup } from './actions';
 export default function LoginPage() {
   return (
     <form>
-      <label htmlFor='email'>Email:</label>
-      <input id='email' name='email' type='email' required />
-      <label htmlFor='password'>Password:</label>
-      <input id='password' name='password' type='password' required />
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" required type="email" />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" required type="password" />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
